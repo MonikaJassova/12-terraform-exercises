ea8fbc0b53dfe335154beba0e5e16feb19da08ec
MonikaJassova <monika.jassova@iohk.io>
2025-12-03 15:55:58 +0100

    fix syntax for helm v3

--- mysql.tf
+++ mysql.tf
@@ -17,13 +17,12 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes {
-    host = data.aws_eks_cluster.cluster.endpoint
-    token = data.aws_eks_cluster_auth.cluster.token
+  kubernetes = {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    token                  = data.aws_eks_cluster_auth.cluster.token
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   }
 }
-
 
 resource "helm_release" "mysql" {
   name = "mysql-release"
