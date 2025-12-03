005779bd47eb78b0858fd5c3b1b67909a75d1055
MonikaJassova <monika.jassova@iohk.io>
2025-12-03 11:51:11 +0100

    fix variable names

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 // values taken from terraform.tfvars file
 variable vpc_cidr_block {}
-variable private_subnet_cidr_block {}
-variable public_subnet_cidr_block {}
+variable private_subnet_cidr_blocks {}
+variable public_subnet_cidr_blocks {}
 variable env_prefix {}
